8694e3099657d9aa433cc0b15c56cf877b232999
Shannon Riester <shannonriester@gmail.com>
2016-06-25 17:24:08 -0500

    found out how to find position of operator in longer string of math equation

--- main.js
+++ main.js
@@ -1,123 +1,155 @@
-// grab the button once pushed
-var equals = document.querySelector('#equals');
-equals.addEventListener('click', function(evt) {
-  console.log(evt);
-});
+var output = document.querySelector('#output');
 
-var addBtn = document.querySelector('#subtract');
-var subtract = document.querySelector('#subtract');
-var multiply = document.querySelector('#multiply');
-var divide = document.querySelector('#divide');
-
-var twoBtn = document.querySelector('#twoBtn');
-var threeBtn = document.querySelector('#threeBtn');
-var fourBtn = document.querySelector('#fourBtn');
-var fiveBtn = document.querySelector('#fiveBtn');
-var sixBtn = document.querySelector('#sixBtn');
-var sevenBtn = document.querySelector('#sevenBtn');
-var eightBtn = document.querySelector('#eightBtn');
-var nineBtn = document.querySelector('#nineBtn');
-var zeroBtn = document.querySelector('#zeroBtn');
-
-
-var oneBtn = document.querySelector('#oneBtn');
+var numBtn = document.querySelector('#oneBtn');
 oneBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 1;
+    // var numOutput = output;
+    output.value += 1;
+    // console.log(output.value, typeof(output.value));
 });
-var twoBtn = document.querySelector('#twoBtn');
+var numBtn = document.querySelector('#twoBtn');
 twoBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 2;
+    // var numOutput = document.querySelector('#output');
+    output.value += 2;
 });
-var threeBtn = document.querySelector('#threeBtn');
+var numBtn = document.querySelector('#threeBtn');
 threeBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 3;
+    // var numOutput = document.querySelector('#output');
+    output.value += 3;
 });
-var fourBtn = document.querySelector('#fourBtn');
+var numBtn = document.querySelector('#fourBtn');
 fourBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 4;
+    // var numOutput = document.querySelector('#output');
+    output.value += 4;
 });
-var fiveBtn = document.querySelector('#fiveBtn');
+var numBtn = document.querySelector('#fiveBtn');
 fiveBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 5;
+    // var numOutput = document.querySelector('#output');
+    output.value += 5;
 });
-var sixBtn = document.querySelector('#sixBtn');
+var numBtn = document.querySelector('#sixBtn');
 sixBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 6;
+    var numOutput = document.querySelector('#output');
+    // output.value = numOutput;
+    output.value += 6;
 });
-var sevenBtn = document.querySelector('#sevenBtn');
+var numBtn = document.querySelector('#sevenBtn');
 sevenBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 7;
+    // var numOutput = document.querySelector('#output');
+    output.value += 7;
 });
-var eightBtn = document.querySelector('#eightBtn');
+var numBtn = document.querySelector('#eightBtn');
 eightBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 8;
+    // var numOutput = document.querySelector('#output');
+    output.value += 8;
 });
-var nineBtn = document.querySelector('#nineBtn');
+var numBtn = document.querySelector('#nineBtn');
 nineBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 9;
+    // var numOutput = document.querySelector('#output');
+    output.value += 9;
 });
-var zeroBtn = document.querySelector('#zeroBtn');
+var numBtn = document.querySelector('#zeroBtn');
 zeroBtn.addEventListener('click', function() {
-  var numOutput = document.querySelector('#output');
-  numOutput.value += 0;
+    // var numOutput = document.querySelector('#output');
+    output.value += 0;
 });
 
-
-var addBtn= document.querySelector('#add');
-var subtractBtn = document.querySelector('#subtract');
-var multiplyBtn = document.querySelector('#multiply');
-var divideBtn = document.querySelector('#divide');
-
-var addBtn = document.querySelector('#addBtn');
-  addBtn.addEventListener('click', function (){
-  var addOutput = document.querySelector('#output');
-  addOutput.value += '+';
+/*operator buttons - - - - - - - - - - */
+var operatorBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', function() {
+    var operatorOutput = document.querySelector('#output');
+    output.value += '+';
 });
-var subtractBtn = document.querySelector('#subtractBtn');
-  subtractBtn.addEventListener('click', function (){
-  var subtractOutput = document.querySelector('#output');
-  subtractOutput.value += '-';
+var operatorBtn = document.querySelector('#subtractBtn');
+subtractBtn.addEventListener('click', function() {
+    var operatorOutput = document.querySelector('#output');
+    output.value += '-';
 });
-var multiplyBtn = document.querySelector('#multiplyBtn');
-  multiplyBtn.addEventListener('click', function (){
-  var multiplyOutput = document.querySelector('#output');
-  multiplyOutput.value += '*';
+var operatorBtn = document.querySelector('#multiplyBtn');
+multiplyBtn.addEventListener('click', function() {
+    var operatorOutput = document.querySelector('#output');
+    output.value += '*';
 });
-var divideBtn = document.querySelector('#divideBtn');
-  divideBtn.addEventListener('click', function (){
-  var divideOutput = document.querySelector('#output');
-  divideOutput.value += '/';
+var operatorBtn = document.querySelector('#divideBtn');
+divideBtn.addEventListener('click', function() {
+    var operatorOutput = document.querySelector('#output');
+    output.value += '/';
+});
+var operatorBtn = document.querySelector('#clearBtn');
+clearBtn.addEventListener('click', function() {
+    var operatorOutput = document.querySelector('#output');
+    output.value = '';
 });
 
+//get the equals button to initiate math process
+var equalsBtn = document.querySelector('#equalsBtn');
+equalsBtn.addEventListener('click', function(evt) {
 
-// Trying to loop the array for the numbers and their onclicks...
 
-// var secondValue = document.getElementsByClassName('numBtn');
-// secondValue.addEventListener('click', function(evt) {
-//     for (i= 0; i<9; i++) {
-//       console.log(evt);
-//       console.log(secondValue[i].value);
-//     }
+    var eqnString = output.value;
+    console.log(eqnString);
+    var input1;
+    var input2;
+    var breakPnt;
+    var breakOpr;
+
+    if (eqnString.includes('+')) {
+      breakPnt = eqnString.indexOf('+');
+      breakOpr = '+';
+      eqnString[2] = breakPnt;
+      console.log(breakPnt);
+    }
+    else if (eqnString.includes('-')) {
+      breakPnt = eqnString.indexOf('-');
+      breakOpr = '-';
+    }
+    else if (eqnString.includes('*')) {
+      breakPnt = eqnString.indexOf('*');
+      breakOpr = '*';
+    }
+    else if (eqnString.includes('/')) {
+      breakPnt = eqnString.indexOf('/');
+      breakOpr = '/';
+    }
+
+
+//     eqnValue.forEach(function (input1, breakOpr, input2) {
+//       console.log(input1, breakOpr, input2);
 // });
 
 
-// zeroBtn.onclick = function(e) {
-//   var secondValue = document.querySelector('#zeroBtn').value;
-//   document.querySelector('#output2').innerText = secondValue;
-//   console.log(e);
-// };
 
-// addBtn.onclick = function(e) {
-//   var operatorValue = document.querySelector('#add').defaultValue;
-//   document.querySelector('#oputOperator').innerText = operatorValue;
-//   console.log(e);
-// };
+
+
+    // for (i = 0; i < eqnArray.length; i++) {
+    //     // - find when i = operator (store in var)
+    //     if (i = ('+' || '-' || '*' || '/')){
+    //         console.log(breakPnt.defaultValue + " : breakPnt");
+    //     }
+    //     else {
+    //         var msgError = "Error!";
+    //         output.innerText = msgError;
+        // }
+        //     - log length of string before operator
+        //     -log length of string after operator
+        //     - take both strings and operate with operator's value
+
+        // var outputArray = output.value;
+        // var userOperator = outputArray[1];
+        // var input1 = outputArray[0];
+        // var input2 = outputArray[2];
+        //
+        // if (outputArray[1] === '+') {
+        //     var sum = Number(input1) + Number(input2);
+        //     output.innerText = sum;
+        // } else if (outputArray[1] === '-') {
+        //     var difference = Number(input1) - Number(input2);
+        //     output.innerText = difference;
+        // } else if (outputArray[1] === '*') {
+        //     var product = Number(input1) * Number(input2);
+        //     output.innerText = product;
+        // } else if (outputArray[1] === '/') {
+        //     var quotient = Number(input1) / Number(input2);
+        //     output.innerText = quotient;
+        // }
+
+});
